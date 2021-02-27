331c1a5be1217d18dd91f55ac94cc8dc6745eb80
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-02-27 17:54:40 +0800

    nEw file with some action and outlets

---
+++ MyLocations/LocationDetailsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LocationDetailsViewController.swift
+//  MyLocations
+//
+//  Created by Giovanni Gaffé on 2021/2/27.
+//
+
+import UIKit
+
+class LocationDetailsViewController: UITableViewController {
+    @IBOutlet var descriptionTextView: UITextView!
+    @IBOutlet var categoryLabel: UILabel!
+    @IBOutlet var latitudeLabel: UILabel!
+    @IBOutlet var longitudeLabel: UILabel!
+    @IBOutlet var adressLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
+    // MARK: - Actions
+    @IBAction func done() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func cancel() {
+        navigationController?.popViewController(animated: true)
+    }
+
+}
