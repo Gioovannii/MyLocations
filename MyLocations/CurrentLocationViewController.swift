8e16b2f0feca0691bef892799c9952da8771b3b3
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-03-10 16:33:32 +0800

    Prepare for segue and change controller properties

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -60,7 +60,13 @@ class CurrentLocationViewController: UIViewController {
     }
     
     // MARK: - Navigation
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "TagLocation" {
+            let controller = segue.destination as! LocationDetailsViewController
+            controller.coordinate = location!.coordinate
+            controller.placemark = placemark
+        }
+    }
     
     // MARK: - Helper Methods
     func showLocationServicesDeniedAlert() {
