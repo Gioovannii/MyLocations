81e4a9aa807dff30b79f5568e359d04921db1615
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-03-12 14:51:58 +0800

    Set array for categories

--- MyLocations/CategoryPickerViewController.swift
+++ MyLocations/CategoryPickerViewController.swift
@@ -9,5 +9,18 @@ import UIKit
 
 class CategoryPickerViewController {
     var selectedCategoryName = ""
-    
+
+    let categories = [
+        "No Categories",
+        "Apple Store",
+        "Bar",
+        "Bookstore",
+        "Club",
+        "Grocery Store",
+        "Historic Building",
+        "House",
+        "Icecream Vendor",
+        "Landmark",
+        "Park"
+    ]
 }
