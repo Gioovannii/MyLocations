258e0d9a51b0c4b102df9acf5848905dfadc8620
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-04-01 22:19:05 +0800

    add save context into scene delegate

--- MyLocations/SceneDelegate.swift
+++ MyLocations/SceneDelegate.swift
@@ -31,6 +31,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }()
     var window: UIWindow?
     
+    // MARK: - Core Data Saving support
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
